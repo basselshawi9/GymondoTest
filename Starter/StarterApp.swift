272e14fffcbb80ea357c72abbf856e11c4407417
tarek.murad <bassel.shawi@gmail.com>
2023-09-22 16:41:39 +0300

    Initial Commit

---
+++ Starter/StarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarterApp.swift
+//  Starter
+//
+//  Created by iMac on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct StarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
